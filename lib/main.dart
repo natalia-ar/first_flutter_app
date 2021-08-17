ee1880f9b207906595c0bb7d3a03e0dd9912229f
Natalia Rodrigues <64341782+natalia-ar@users.noreply.github.com>
2021-08-17 09:54:07 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:math';
 //ctrl + clique para ver a biblioteca
 
 void main() {
@@ -21,13 +22,22 @@ class HomeStateful extends StatefulWidget {
 class _HomeStatefulState extends State<HomeStateful> {
   final _style = TextStyle(fontSize: 22, fontWeight: FontWeight.w400);
   var _frases = [
+
     "Nada é pequeno se feito com amor.",
     "A medida do amor é amar sem medida.",
     "Nunca compreenderemos o quanto um simples sorriso pode fazer.",
     "Todo mal é ausência de amor.",
     "Comece fazendo o que é necessário, depois o que é possível, e de repente você estará fazendo o impossível."
   ];
-  var _botaoFrase = "Clique no botão acima para gerar uma nova frase!";
+  var _botaoFrase = "Clique no botão para gerar uma nova frase!";
+
+  void _gerarFrase() {
+    var numeroSorteado = Random().nextInt(_frases.length);
+      setState(() {
+        _botaoFrase = _frases[numeroSorteado];
+      });
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,15 +50,16 @@ class _HomeStatefulState extends State<HomeStateful> {
             textStyle: _style
           ),
           ),
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.deepPurple,
       ),
       body: Container (
         padding: EdgeInsets.all(16),
         width: double.infinity,
         decoration: BoxDecoration(
-          border: Border.all(width: 3, color: Colors.green)
+          border: Border.all(width: 3, color: Colors.deepPurple)
         ),
         child: Column(
+
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Image.asset(
@@ -56,23 +67,26 @@ class _HomeStatefulState extends State<HomeStateful> {
               alignment: Alignment.center,
             ),
 
-            RaisedButton(onPressed: (){
+            Text(_botaoFrase,
+              textAlign: TextAlign.center,
+              style: _style,),
 
-            },
-              color: Colors.green,
-              child: Text("nova frase",
-              style:TextStyle(
-                fontSize: 20
-              ),),
+            Padding(
+                padding: EdgeInsets.only(top: 60)
             ),
 
-            Text(_botaoFrase,
-            textAlign: TextAlign.center,
-            style: _style,)
+            ElevatedButton(
+
+              onPressed: _gerarFrase,
+              child: Text("nova frase",
+              style:
+              TextStyle(
+                fontSize: 20
+              ),),
+            )
           ],
         ),
       )
-
     );
   }
 }
